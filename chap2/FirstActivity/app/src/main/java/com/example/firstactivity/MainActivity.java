6ecbca67ac5557de06fa7477cff4706af8a58fd6
zpwlow <315320819@qq.com>
2020-09-20 14:51:29 +0800

    探究活动，两个活动通过两个按钮来回切换。

--- chap2/FirstActivity/app/src/main/java/com/example/firstactivity/MainActivity.java
+++ chap2/FirstActivity/app/src/main/java/com/example/firstactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.firstactivity;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -16,10 +19,28 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, "you clicked button 1",
-                               Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "you clicked button 1",Toast.LENGTH_SHORT).show();
+                String data = "Hello SecondActivity";
+                Intent intent =new Intent(MainActivity.this,SecondActivity.class);//打开按钮二界面
+                intent.putExtra("extra_data",data);
+                startActivityForResult(intent,1);
+                //startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FistActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     @Override
@@ -28,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item){ //创建目录item
         switch (item.getItemId()){
             case R.id.add_item:
                 Toast.makeText(this,"You clicked add",Toast.LENGTH_SHORT).show();
--- chap2/FirstActivity/app/src/main/java/com/example/firstactivity/SecondActivity.java
+++ chap2/FirstActivity/app/src/main/java/com/example/firstactivity/SecondActivity.java
@@ -2,13 +2,43 @@ package com.example.firstactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.second_layout);
+        Intent intent1 = getIntent();
+        String data = intent1.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Intent intent = new Intent(Intent.ACTION_VIEW); //打开百度网页
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+                //Intent intent = new Intent(Intent.ACTION_DIAL); //使用电话拨打电话
+                //intent.setData(Uri.parse("tel:10086"));
+                //startActivity(intent);
+                Intent intent = new Intent();//给上一个活动传数据
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
