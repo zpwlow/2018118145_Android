be1a345c45a09558e2ebdc38cfe5f3824509c6d1
zpwlow <315320819@qq.com>
2020-09-20 10:55:32 +0800

    重构文件路径，进行chap2  Activity 代码练习。

---
+++ chap2/FirstActivity/app/src/main/java/com/example/firstactivity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.firstactivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "you clicked button 1",
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
