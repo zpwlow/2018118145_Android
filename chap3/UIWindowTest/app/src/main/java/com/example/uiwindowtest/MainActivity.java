c45752f9717d26c60c7215a4c0c12bc23be92381
zpwlow <315320819@qq.com>
2020-09-21 21:47:25 +0800

    UI界面的按钮尝试

--- chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/MainActivity.java
+++ chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/MainActivity.java
@@ -3,14 +3,34 @@ package com.example.uiwindowtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        button.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this,
+                               inputText,Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
