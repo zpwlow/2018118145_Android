9f8764afb0a1d7474cfa6a354ce3451da2504155
zpwlow <315320819@qq.com>
2020-09-22 12:38:22 +0800

    尝试UI插入图片

--- chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/MainActivity.java
+++ chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnClickListener {
     private EditText editText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener((View.OnClickListener) this);
     }
 
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity
     public void onClick(View v){
         switch (v.getId()){
             case R.id.button:
+                imageView.setImageResource(R.drawable.img_2);
                 String inputText = editText.getText().toString();
                 Toast.makeText(MainActivity.this,
                                inputText,Toast.LENGTH_SHORT).show();
