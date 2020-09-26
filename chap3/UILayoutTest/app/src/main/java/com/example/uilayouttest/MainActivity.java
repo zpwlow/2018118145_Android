2e667104b3eb6bcc5859d4108a5bb826b009018f
zpwlow <315320819@qq.com>
2020-09-26 20:56:52 +0800

    水平布局

--- chap3/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
+++ chap3/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.helloworld;
+package com.example.uilayouttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.helloworld.R;
 
 public class MainActivity extends AppCompatActivity {
 
