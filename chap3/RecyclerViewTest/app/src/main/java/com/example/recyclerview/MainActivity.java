76b849294c21010be91c096fccc13d128c3d4a0b
zpwlow <315320819@qq.com>
2020-10-02 12:52:42 +0800

    RecylcerView 的瀑布流滚动

--- chap3/RecyclerViewTest/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ chap3/RecyclerViewTest/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initCarLogo(); //初始化汽车标志
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager layoutManager = new
+                StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(layoutManager);
         CarLogoAdapter adapter = new CarLogoAdapter(carLogoList);
         recyclerView.setAdapter(adapter);
@@ -28,30 +32,47 @@ public class MainActivity extends AppCompatActivity {
 
     private void initCarLogo(){
         for(int i=0;i<2;i++){
-            CarLogo aodi = new CarLogo("奥迪",R.drawable.aodi_pic);
+            //CarLogo aodi = new CarLogo("奥迪",R.drawable.aodi_pic);
+            CarLogo aodi = new CarLogo(getRandomLengthName("奥迪 "),R.drawable.aodi_pic);
             carLogoList.add(aodi);
-            CarLogo baoma = new CarLogo("宝马",R.drawable.baoma_pic);
+            //CarLogo baoma = new CarLogo("宝马",R.drawable.baoma_pic);
+            CarLogo baoma = new CarLogo(getRandomLengthName("宝马 "),R.drawable.baoma_pic);
             carLogoList.add(baoma);
-            CarLogo baoshijie = new CarLogo("保时捷",R.drawable.baoshijie_pic);
+            //CarLogo baoshijie = new CarLogo("保时捷",R.drawable.baoshijie_pic);
+            CarLogo baoshijie = new CarLogo(getRandomLengthName("保时捷 "),R.drawable.baoshijie_pic);
             carLogoList.add(baoshijie);
-            CarLogo benchi = new CarLogo("奔驰",R.drawable.benchi_pic);
+            //CarLogo benchi = new CarLogo("奔驰",R.drawable.benchi_pic);
+            CarLogo benchi = new CarLogo(getRandomLengthName("奔驰 "),R.drawable.benchi_pic);
             carLogoList.add(benchi);
-            CarLogo binli = new CarLogo("宾利",R.drawable.binli_pic);
+            //CarLogo binli = new CarLogo("宾利",R.drawable.binli_pic);
+            CarLogo binli = new CarLogo(getRandomLengthName("宾利 "),R.drawable.binli_pic);
             carLogoList.add(binli);
-            CarLogo falali = new CarLogo("法拉利",R.drawable.falali_pic);
+            //CarLogo falali = new CarLogo("法拉利",R.drawable.falali_pic);
+            CarLogo falali = new CarLogo(getRandomLengthName("法拉利 "),R.drawable.falali_pic);
             carLogoList.add(falali);
-            CarLogo fute = new CarLogo("福特",R.drawable.fute_pic);
+            //CarLogo fute = new CarLogo("福特",R.drawable.fute_pic);
+            CarLogo fute = new CarLogo(getRandomLengthName("福特 "),R.drawable.fute_pic);
             carLogoList.add(fute);
-            CarLogo kaidilake = new CarLogo("凯迪拉克",R.drawable.kaidilake_pic);
+            CarLogo kaidilake = new CarLogo(getRandomLengthName("凯迪拉克 "),R.drawable.kaidilake_pic);
             carLogoList.add(kaidilake);
-            CarLogo lanbojini = new CarLogo("兰博基尼",R.drawable.lanbojini_pic);
+            CarLogo lanbojini = new CarLogo(getRandomLengthName("兰博基尼 "),R.drawable.lanbojini_pic);
             carLogoList.add(lanbojini);
-            CarLogo leikesasi = new CarLogo("雷克萨斯",R.drawable.leikesasi_pic);
+            CarLogo leikesasi = new CarLogo(getRandomLengthName("雷克萨斯 "),R.drawable.leikesasi_pic);
             carLogoList.add(leikesasi);
-            CarLogo luhu = new CarLogo("路虎",R.drawable.luhu_pic);
+            CarLogo luhu = new CarLogo(getRandomLengthName("路虎 "),R.drawable.luhu_pic);
             carLogoList.add(luhu);
-            CarLogo mashaladi = new CarLogo("玛莎拉蒂",R.drawable.mashaladi_pic);
+            CarLogo mashaladi = new CarLogo(getRandomLengthName("玛莎拉蒂 "),R.drawable.mashaladi_pic);
             carLogoList.add(mashaladi);
         }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20)+1;
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<length;i++){
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
