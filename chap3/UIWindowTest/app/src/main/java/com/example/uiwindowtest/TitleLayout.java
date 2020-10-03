4ff0e17740e62801a7941818fe87e0a2ee9fb3b0
zpwlow <315320819@qq.com>
2020-10-03 17:30:54 +0800

    重新修改实验的界面内容

--- chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/TitleLayout.java
+++ chap3/UIWindowTest/app/src/main/java/com/example/uiwindowtest/TitleLayout.java
@@ -12,14 +12,12 @@ import android.widget.Toast;
 
 class TitleLayout extends LinearLayout {
 
-    private ImageView imageView;
 
     public TitleLayout(Context context, AttributeSet attrs) {
         super(context,attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
         Button titleBack = (Button)findViewById(R.id.title_back);
         Button titlenext = (Button) findViewById(R.id.title_next);
-        imageView = (ImageView) findViewById(R.id.image_view);
         titleBack.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
