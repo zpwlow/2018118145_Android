cd593c5d4befe54c0d92bb9dc5bf0aba18be0f3f
zpwlow <315320819@qq.com>
2020-10-16 22:55:38 +0800

    自定义广播

--- chap5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
+++ chap5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -5,11 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class BootCompleteReceiver extends BroadcastReceiver {
+public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "系统开机完成！", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "成功接收自定义全局广播！", Toast.LENGTH_LONG).show();
+        abortBroadcast();
     }
 
 }
