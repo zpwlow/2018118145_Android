7165ab86483d52eae57b339ca48409a4689e3671
zpwlow <315320819@qq.com>
2020-10-17 11:19:33 +0800

    编辑登录的界面布局

--- chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -1,7 +1,17 @@
 package com.example.broadcastbestpractice;
 
+import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends BaseActivity{
     private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle sevedInstanceState){
+        super.onCreate(sevedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
