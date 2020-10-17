927b19473406949016e32633dddf56a066321e4e
zpwlow <315320819@qq.com>
2020-10-17 18:51:15 +0800

    将数据存储到 SharedPreferences

--- chap6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ chap6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sharedpreferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button savedata = (Button) findViewById(R.id.save_data);
+        savedata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor =
+                        getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age",28);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
     }
 }
