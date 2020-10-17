7b0a6f6bbaa764f700fb3713c6b137249c9f3f97
zpwlow <315320819@qq.com>
2020-10-17 11:34:26 +0800

    主界面加入强制下线的功能

--- chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.broadcastbestpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity{
     private EditText accountEdit;
@@ -13,5 +16,22 @@ public class LoginActivity extends BaseActivity{
     protected void onCreate(Bundle sevedInstanceState){
         super.onCreate(sevedInstanceState);
         setContentView(R.layout.activity_login);
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (account.equals("zhong") && password.equals("123456")){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this,"账号或者密码错误！",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
+++ chap5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.broadcastbestpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.breadcastbestpratice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
     }
 }
