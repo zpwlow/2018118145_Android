58ad030a9447b82caac1fc87da3c92196a419337
zpwlow <315320819@qq.com>
2020-10-18 14:09:22 +0800

    创建数据库

--- chap6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ chap6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.databasetest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbhelper = new MyDatabaseHelper(this,"BookStore.db",null,1);
+        Button createbase = (Button) findViewById(R.id.create_database);
+        createbase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbhelper.getWritableDatabase();
+            }
+        });
     }
 }
