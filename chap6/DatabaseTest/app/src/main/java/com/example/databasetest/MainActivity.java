bd8be525616482ad1560b59e602dfd252ee42569
zpwlow <315320819@qq.com>
2020-10-18 14:29:16 +0800

    添加数据进数据库

--- chap6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ chap6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.databasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dbhelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db  = dbhelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Browm");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values); //插入第一条数据
+                values.clear();
+                // 插入第二条数据
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Browm");
+                values.put("pages",510);
+                values.put("price",19.95);
+                db.insert("Book",null,values);
+
             }
         });
     }
