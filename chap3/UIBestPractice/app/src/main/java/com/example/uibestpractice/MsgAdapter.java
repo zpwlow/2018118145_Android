d088a0b784ca079a113b32a69085bfcbd628e32e
zpwlow <315320819@qq.com>
2020-10-19 18:46:03 +0800

    创建 RecyclerView 的适配器

---
+++ chap3/UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.java
@@ -0,0 +1,67 @@
+package com.example.uibestpractice;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+    private List<Msg> mMsgList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            leftLayout = (LinearLayout) itemView.findViewById(R.id.left_layout);
+            rightLayout = (LinearLayout) itemView.findViewById(R.id.right_layout);
+            leftMsg = (TextView) itemView.findViewById(R.id.left_mag);
+            rightMsg = (TextView) itemView.findViewById(R.id.right_mag);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList){
+        mMsgList = msgList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                    int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MsgAdapter.ViewHolder holder,
+                                 int position) {
+        Msg msg = mMsgList.get(position);
+        if(msg.getType() == Msg.TYPR_RECEIVED){
+            //如果是收到消息，则显示左边的消息布局，将右边的消息布局隐藏。
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        }else if(msg.getType() == Msg.TYPE_SENT){
+            //如果是发出的消息，则显示右边的消息布局，将左边的消息布局隐藏。
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightMsg.setText(msg.getContent());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
+}
