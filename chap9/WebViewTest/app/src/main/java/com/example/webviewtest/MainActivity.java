5637530d33ebf96c3c95c2ae76640483a7b06a26
zpwlow <315320819@qq.com>
2020-11-02 16:36:32 +0800

    修改运行时发现的bug

--- chap9/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
+++ chap9/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.webviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView)findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://www.baidu.com");
     }
 }
