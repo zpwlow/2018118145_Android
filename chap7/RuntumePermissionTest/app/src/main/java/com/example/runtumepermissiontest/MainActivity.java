42c675d27596c1186c7f951a46003cec47928f4f
zpwlow <315320819@qq.com>
2020-11-02 16:00:50 +0800

    修改MainActivity.java文件内容,点击按钮触发拨打电话

--- chap7/RuntumePermissionTest/app/src/main/java/com/example/runtumepermissiontest/MainActivity.java
+++ chap7/RuntumePermissionTest/app/src/main/java/com/example/runtumepermissiontest/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.runtumepermissiontest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button makecall = (Button)findViewById(R.id.make_call);
+        makecall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tell:10086"));
+                    startActivity(intent);
+                }catch (SecurityException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
