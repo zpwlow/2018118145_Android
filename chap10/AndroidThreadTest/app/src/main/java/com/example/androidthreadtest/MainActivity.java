162d4522cd8ed87d1f336d3d19b414dd4b49001e
zpwlow <315320819@qq.com>
2020-11-10 21:49:33 +0800

    发送异步消息

--- chap10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
+++ chap10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.androidthreadtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int UPDATE_TEXT = 1;
+
     private TextView text;
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    //在这里可以进行UI操作
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +47,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        text.setText("Nice to meet you");
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);//将Message对象发送出去
                     }
                 }).start();
                 break;
