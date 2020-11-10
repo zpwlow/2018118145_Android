60161aca0939b012e80bccc38d61413b26621515
zpwlow <315320819@qq.com>
2020-11-10 21:42:51 +0800

    修改主活动文件内容

--- chap10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
+++ chap10/AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.androidthreadtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        Button changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText("Nice to meet you");
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
