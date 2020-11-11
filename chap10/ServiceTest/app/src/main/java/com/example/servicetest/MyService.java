6ccdbb2260125a149c9856ff3ad609133993f325
zpwlow <315320819@qq.com>
2020-11-11 20:12:01 +0800

    前台服务

--- chap10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ chap10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,10 +1,15 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
 
@@ -36,6 +41,18 @@ public class MyService extends Service {
     public void onCreate(){
         super.onCreate();
         Log.d("MyService","onCreate executed");
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,
+                      0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this)
+                          .setContentTitle("This is content title")
+                          .setContentText("this is content text")
+                          .setWhen(System.currentTimeMillis())
+                          .setSmallIcon(R.mipmap.ic_launcher)
+                          .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                                  R.mipmap.ic_launcher))
+                          .setContentIntent(pi).build();
+        startForeground(1,notification);
     }
 
     @Override
