4f742629243a70014b38585f233c687801cc0af4
zpwlow <315320819@qq.com>
2020-11-11 21:40:24 +0800

    增加布局

--- chap10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ chap10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
