caa95c44637b3cb5a310831974c57eab89a4065c
zpwlow <315320819@qq.com>
2020-11-11 20:37:43 +0800

    修改前台服务8.0以上的版本配置不一样

--- chap10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ chap10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,10 +1,13 @@
 package com.example.servicetest;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -26,7 +29,6 @@ public class MyService extends Service {
         }
     }
 
-
     public MyService() {
     }
 
@@ -41,10 +43,24 @@ public class MyService extends Service {
     public void onCreate(){
         super.onCreate();
         Log.d("MyService","onCreate executed");
+        String CHANNEL_ONE_ID = "com.primedu.cn";
+        String CHANNEL_ONE_NAME = "Channel One";
+        NotificationChannel notificationChannel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel(CHANNEL_ONE_ID,
+                    CHANNEL_ONE_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(notificationChannel);
+        }
         Intent intent = new Intent(this,MainActivity.class);
         PendingIntent pi = PendingIntent.getActivity(this,
                       0,intent,0);
         Notification notification = new NotificationCompat.Builder(this)
+                          .setChannelId(CHANNEL_ONE_ID)
                           .setContentTitle("This is content title")
                           .setContentText("this is content text")
                           .setWhen(System.currentTimeMillis())
@@ -52,7 +68,8 @@ public class MyService extends Service {
                           .setLargeIcon(BitmapFactory.decodeResource(getResources(),
                                   R.mipmap.ic_launcher))
                           .setContentIntent(pi).build();
-        startForeground(1,notification);
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+        startForeground(1, notification);
     }
 
     @Override
