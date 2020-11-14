629fdcbe18d52f6d4bcd48496424dd05298438cc
zpwlow <315320819@qq.com>
2020-11-14 11:19:04 +0800

    使用通知

--- chap8/NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ chap8/NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.notificationtest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.send_notice:
+                String CHANNEL_ONE_ID = "com.primedu.cn";
+                String CHANNEL_ONE_NAME = "Channel One";
+                NotificationChannel notificationChannel = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    notificationChannel = new NotificationChannel(CHANNEL_ONE_ID,
+                            CHANNEL_ONE_NAME, NotificationManager.IMPORTANCE_HIGH);
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    notificationChannel.setShowBadge(true);
+                    notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+                    NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    manager.createNotificationChannel(notificationChannel);
+                }
+                NotificationManager manager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new Notification.Builder(this)
+                        .setChannelId(CHANNEL_ONE_ID)
+                        .setContentTitle("This is context title")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources()
+                                ,R.mipmap.ic_launcher))
+                        .build();
+                manager.notify(1,notification);
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
 }
