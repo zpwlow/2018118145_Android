62e0dfdcd8e5bc8d7729f0aa57828346be8ffaaa
zpwlow <315320819@qq.com>
2020-11-15 17:41:55 +0800

    编写实验一的文档

---
+++ chap1/HelloWorld/工具链安装和运行Helloworld截图.md
@@ -0,0 +1,134 @@
+# 工具链的安装和运行helloword程序
+
+## 一、工具链的安装
+
+### 1、GitHub
+
+SSH授权管理：
+
+(1)、点击GitHub个人头像，进入Setting，并选择SSH和GPG keys:
+
+![image-20201115172309399](../image/image-20201115172309399.png) 
+
+ 
+
+(2)、点击New SSH key:
+
+![image-20201115172351701](../image/image-20201115172351701.png)
+
+(3)、在Title位置填写一个合适的名称，然后打开SourceTree，点击工具，选择创建或导入SSH密钥：
+
+<img src="../image/image-20201115172608745.png" alt="image-20201115172608745" style="zoom:67%;" />
+
+(4)、点击Generate：
+
+![img](../image/wps4.jpg)![img](../image/wps5.jpg) 
+
+ 
+
+(5)、设置密码，然后点击Save private key,保存PPK文件
+
+![img](../image/wps6.jpg) 
+
+
+
+(6)、复制公钥，并粘贴到GitHub中：
+
+![img](../image/wps8.jpg) 
+
+ 
+
+![img](../image/wps9.jpg) 
+
+### 2、SourceTree：
+
+软件下载网址：https://www.sourcetreeapp.com/
+
+下载好软件安装后。
+
+连接到云端的GitHub：
+
+1、 点击SourceTree右上角的设置：
+
+![image-20201115172730921](../image/image-20201115172730921.png)
+
+2、 点击添加，并打开GitHub，选择克隆并复制SSH：
+
+![image-20201115172908395](../image/image-20201115172908395.png)
+
+ 
+
+ 
+
+3、 将复制好的SSH粘贴到URL并点击确定：
+
+<img src="../image/image-20201115172943621.png" alt="image-20201115172943621"  />
+
+### 3、MarkDown：
+
+Typora软件下载地址：https://typora.io/
+
+1、 下载并安装好Typora:
+
+![image-20201115173251075](../image/image-20201115173251075.png)
+
+### 4、Android Studio
+
+安装的过程如下图的顺序。
+
+![img](../image/wps13.jpg) 
+
+![img](../image/wps14.jpg) 
+
+![img](../image/wps15.jpg) 
+
+![img](../image/wps16.jpg) 
+
+![img](../image/wps17.jpg) 
+
+![img](../image/wps18.jpg) 
+
+![img](../image/wps19.jpg) 
+
+![img](../image/wps20.jpg) 
+
+![img](../image/wps21.jpg) 
+
+![img](../image/wps22.jpg) 
+
+![img](../image/wps23.jpg) 
+
+![img](../image/wps24.jpg) 
+
+![img](../image/wps25.jpg) 
+
+![img](../image/wps26.jpg) 
+
+C:\Users\zpw\AppData\Local\Android\Sdk
+
+![img](../image/wps27.jpg) 
+
+![img](../image/wps28.jpg) 
+
+![img](../image/wps29.jpg) 
+
+![img](../image/wps30.jpg) 
+
+![img](../image/wps31.jpg) 
+
+![img](../image/wps35.jpg) 
+
+![img](../image/wps36.jpg) 
+
+## 二、运行helloworld程序
+
+创建一个helloworld的项目，运行程序如下图所示。
+
+ ![image-20201115173805742](../image/image-20201115173805742.png)
+
+![img](../image/wps33.jpg) 
+
+![img](../image/wps34.jpg) 
+
+<img src="../image/wps37.jpg" alt="img" style="zoom:67%;" /> 
+
--- chap8/ComeraAlbumTest/app/src/main/java/com/example/comeraalbumtest/MainActivity.java
+++ chap8/ComeraAlbumTest/app/src/main/java/com/example/comeraalbumtest/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.comeraalbumtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +29,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
     private ImageView picture;
     private Uri imageUri;
 
@@ -29,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button takePhoto = (Button) findViewById(R.id.take_photo);
+        Button chooseFromAlum = (Button) findViewById(R.id.choose_form_album);
         picture = (ImageView) findViewById(R.id.picture);
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +67,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,TAKE_PHOTO);
             }
         });
+
+        chooseFromAlum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1);
+                }else{
+                    openAlbum();
+                }
+            }
+        });
+    }
+
+    private  void openAlbum(){
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);//打开相册
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this,
+                            "You denies the permission",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 
     @Override
@@ -64,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case TAKE_PHOTO:
-                if (requestCode == RESULT_OK) {
+                if (resultCode == RESULT_OK) {
                     try {
                         //将拍摄的照片显示出来
                         Bitmap bitmap = BitmapFactory.
@@ -76,8 +125,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19){
+                        //4.4及以上的系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    }else {
+                        //4.4以下的系统使用这个方法处理图片
+                        handleImageBoforeKitKat(data);
+                    }
+                    break;
+                }
             default:
                 break;
         }
+    }
+
+    @TargetApi(19)
+    private  void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            //如果时document 类型的Uri,则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.meida.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1] //解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" +id;
+                imagePath = getImagePath(MediaStore.Images
+                        .Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents"
+                    .equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.
+                        withAppendedId(Uri.parse("content://downloads/public_downloads")
+                                ,Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+       }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是content类型的Uri,则使用普通方式处理
+            imagePath = getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是file 类型的Uri,直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);//根据图片路径显示图片
     }
 }
