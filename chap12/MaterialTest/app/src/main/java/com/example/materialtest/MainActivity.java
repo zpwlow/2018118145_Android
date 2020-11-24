f1b48bf58e50d7e7bf0dd813136f5d4d629ad65a
zpwlow <315320819@qq.com>
2020-11-24 22:12:45 +0800

    添加菜单文件

--- chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You clicked delete",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"You clicked Settings",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 
 
