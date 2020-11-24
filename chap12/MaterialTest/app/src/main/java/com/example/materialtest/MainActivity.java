f89f29a3c65887726110a4f532386a50ac486528
zpwlow <315320819@qq.com>
2020-11-24 21:56:51 +0800

    运行ToolBar界面

--- chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.materialtest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
+
 }
