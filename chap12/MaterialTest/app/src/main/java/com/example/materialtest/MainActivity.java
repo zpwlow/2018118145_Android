e961296a23513359c3222cd59652e626ef58ccb8
zpwlow <315320819@qq.com>
2020-11-27 19:58:31 +0800

    增加悬浮按钮

--- chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ chap12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -12,8 +12,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 
@@ -41,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "FAB clicked",Toast.LENGTH_SHORT).show();
             }
         });
     }
