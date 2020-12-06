fc69d1986a5d1d3d9cdfbfb79170fcd1dd4c21da
zpwlow <315320819@qq.com>
2020-12-06 16:44:34 +0800

    主布局

---
+++ BigJob/StickyNotes/app/src/main/java/com/example/stickynotes/ui/home/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.example.stickynotes.ui.home;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import com.example.stickynotes.R;
+import com.example.stickynotes.databinding.FragmentHomeBinding;
+import com.example.stickynotes.model.Dbservice;
+import com.example.stickynotes.util.TranNum;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        homeViewModel = new ViewModelProvider(requireActivity()).
+                get(HomeViewModel.class);
+
+        FragmentHomeBinding binding;
+        binding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_home, container, false);
+        binding.setData(homeViewModel);
+        binding.setLifecycleOwner(getActivity());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+}
---
+++ BigJob/StickyNotes/app/src/main/java/com/example/stickynotes/ui/home/HomeViewModel.java
@@ -0,0 +1,97 @@
+package com.example.stickynotes.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.stickynotes.model.GSON.One;
+import com.example.stickynotes.model.OneHome;
+import com.example.stickynotes.util.TranNum;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HomeViewModel extends ViewModel {
+
+
+    public MutableLiveData<OneHome> oneHome;
+
+    public MutableLiveData<One> getOne() {
+        if (one == null) {
+            one = new MutableLiveData<>();
+        }
+
+        return one;
+    }
+
+    public void changeLogDay() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://v1.hitokoto.cn/")
+                        .build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    String jsondata = response.body().string();
+
+                    Gson gson = new Gson();
+                    One oneN = gson.fromJson(jsondata, One.class);
+                    if (!(oneN.getFrom().matches("[\\u4e00-\\u9fa5]+"))){
+                        oneN.setFrom("佚名");
+                    }
+                    one.postValue(oneN);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void setOne(MutableLiveData<One> one) {
+        this.one = one;
+    }
+
+    public MutableLiveData<One> one;
+
+
+    public HomeViewModel() {
+        changeLogDay();
+        changeTime();
+    }
+
+
+    public MutableLiveData<OneHome> getOneHome() {
+        if (oneHome == null) {
+            oneHome = new MutableLiveData<>();
+        }
+
+        return oneHome;
+    }
+
+    public void setOneHome(MutableLiveData<OneHome> oneHome) {
+        this.oneHome = oneHome;
+    }
+
+    public void changeTime() {
+        if (oneHome == null) {
+            oneHome = new MutableLiveData<>();
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");// HH:mm:ss
+        SimpleDateFormat simpleDateFormatHour = new SimpleDateFormat("HH");
+//获取当前时间
+        Date date = new Date(System.currentTimeMillis());
+        OneHome oneHomev = new OneHome();
+        oneHomev.setTime(TranNum.transHour(simpleDateFormatHour.format(date)));
+        oneHomev.setDate(TranNum.transNum(simpleDateFormat.format(date)));
+
+
+        oneHome.postValue(oneHomev);
+    }
+
+}
