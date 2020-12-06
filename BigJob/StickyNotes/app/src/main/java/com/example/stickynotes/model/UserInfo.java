02f38dbcb1057410b870bdf5b900459ed7c891b9
zpwlow <315320819@qq.com>
2020-12-06 11:53:00 +0800

    完成UserInfo类文件

--- BigJob/StickyNotes/app/src/main/java/com/example/stickynotes/model/UserInfo.java
+++ BigJob/StickyNotes/app/src/main/java/com/example/stickynotes/model/UserInfo.java
@@ -1,4 +1,42 @@
 package com.example.stickynotes.model;
 
-public class UserInfo {
+import org.litepal.crud.DataSupport;
+
+public class UserInfo extends DataSupport {
+    private String name;
+    private String saying;
+    private  String profile;
+    private  boolean firstFlag;
+
+    public boolean isFirstFlag() {
+        return firstFlag;
+    }
+
+    public void setFirstFlag(boolean firstFlag) {
+        this.firstFlag = firstFlag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
 }
