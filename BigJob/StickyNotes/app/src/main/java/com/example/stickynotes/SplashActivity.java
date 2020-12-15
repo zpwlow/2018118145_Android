4edab12e3f54184ca6aa34ff3ee22f4536710b20
zpwlow <315320819@qq.com>
2020-12-15 23:08:13 +0800

    增加软件的启动画面

---
+++ BigJob/StickyNotes/app/src/main/java/com/example/stickynotes/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.stickynotes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_DISPLAY_LENGHT= 1000;    //延迟1秒
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);//去掉标题
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);	//第二个参数即为执行完跳转后的Activity
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();   //关闭splashActivity，将其回收，否则按返回键会返回此界面
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
